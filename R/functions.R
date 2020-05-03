b1204d1e6224b75e7fce37e235a912cfc3ee47d6
PeterAnthonyD <peteradola@gmail.com>
2020-05-03 15:52:45 -0400

    Adjusted description and ncol.

--- R/functions.R
+++ R/functions.R
@@ -1,9 +1,9 @@
-#' Create a linear model between y and x(s). This also creates a scatterplot of the y and x variables.
+#' Create a linear model between y and x(s). For data with less than five (5) x variables, a scattergram of all variables will be created.
 #'
 #' This is truly a great and much-needed function
 #'
 #' @param x A vector or matrix of data to predict y
-#' @param y A vector of numbers
+#' @param y A vector or matrix of numbers
 #' @param sub The number of rows in the data
 #' @return Scattergram of variables. Coeffecients and p-values of linear model.
 #' @export
@@ -13,7 +13,7 @@
 myLinearRegression <- function(x=NULL, y=NULL , sub=NULL){
   x<-as.data.frame(x)
   theData<-as.data.frame(cbind(y,x))
-  if (ncol(theData) < 6) {
+  if (ncol(theData) < 7) {
     p<-ggpairs(data = theData, columns = 1:ncol(theData), title = "Scatterplots of Variables")
   } else {
     p<-ggplot(theData) + labs(title="Too Many Variables")
